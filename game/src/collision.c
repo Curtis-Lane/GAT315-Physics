cd03ab672a77cc8a3673e1310062e89c0c658900
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-05-16 23:03:18 -0600

    Added comments to the GenerateContact and SeparateContacts methods

--- game/src/collision.c
+++ game/src/collision.c
@@ -36,37 +36,59 @@ void CreateContacts(ncBody* bodies, ncContact** contacts) {
 }
 
 ncContact* GenerateContact(ncBody* bodyOne, ncBody* bodyTwo) {
+	// Allocate memory for a new contact
 	ncContact* contact = (ncContact*) malloc(sizeof(ncContact));
+	// Ensure that memory was allocated successfully
 	assert(contact != NULL);
+	// Overwrite the memory with zeros so that we don't end up using the random junk data that happened to exist in that memory
 	memset(contact, 0, sizeof(ncContact));
 
+	// Set the contact's two bodies
 	contact->bodyOne = bodyOne;
 	contact->bodyTwo = bodyTwo;
 
+	// Calculate the direction vector of the contact
 	Vector2 direction = Vector2Subtract(bodyOne->position, bodyTwo->position);
+	// Calculate the distance between the bodies
 	float distance = Vector2Length(direction);
+	// Ensure that the direction can never equal zero, so that we avoid a potential divide by zero error
 	if(distance == 0) {
 		direction = CreateVector2(GetRandomFloatValue(-0.05f, 0.05f), GetRandomFloatValue(-0.05f, 0.05f));
 	}
 
+	// Get the combined radii of the two bodies
+	// In this case, that would be the combined mass of the bodies, since that is what we are currently using for the radius
 	float radius = (bodyOne->mass + bodyTwo->mass);
 
+	// Calculate the depth of the penetration
 	contact->depth = radius - distance;
+	// Normalize the direction vector to create the contact's normal vector
 	contact->normal = Vector2Normalize(direction);
+	// Calculate the combined restitution of the two bodies
 	contact->restitution = (bodyOne->restitution + bodyTwo->restitution) * 0.5f;
 
+	// Return the newly created contact
 	return contact;
 }
 
 void SeparateContacts(ncContact* contacts) {
+	// Iterate over every contact that was provided
 	for(ncContact* contact = contacts; contact != NULL; contact = contact->next) {
+		// Calculate the combined inverse mass of both of the contact's bodies
 		float totalInvMass = contact->bodyOne->inverseMass + contact->bodyTwo->inverseMass;
+
+		// Calculate the direction that would most directly separate the two bodies
+		// This is done by scaling the normal vector of the contact by the depth of the contact divided by the total inverse mass
 		Vector2 seperation = Vector2Scale(contact->normal, contact->depth / totalInvMass);
+
+		// Move each body so that they are no longer overlapping
+		// The second body is sent in the opposite direction of the separate vector so that the two bodies are sent in opposing directions
 		contact->bodyOne->position = Vector2Add(contact->bodyOne->position, Vector2Scale(seperation, contact->bodyOne->inverseMass));
 		contact->bodyTwo->position = Vector2Add(contact->bodyTwo->position, Vector2Negate(Vector2Scale(seperation, contact->bodyTwo->inverseMass)));
 	}
 }
 
+// TODO: Add Comments
 void ResolveContacts(ncContact* contacts) {
 	for(ncContact* contact = contacts; contact != NULL; contact = contact->next) {
 		Vector2 relativeVelocity = Vector2Subtract(contact->bodyOne->velocity, contact->bodyTwo->velocity);
